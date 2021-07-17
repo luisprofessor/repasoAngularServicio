28d6896bc3471dd9a6a9422f2be4c780f8144dc8
luisprofessor@gmail.com <luisprofessor@gmail.com>
2021-07-17 19:31:55 -0300

    todo

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  mensajes:any[]=[];
+  subscribe:any;
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+
+    this.subscribe=this.dataService.getPost()
+    .subscribe((posts: any) =>{
+        this.mensajes=posts;
+        console.log(posts);
+      }
+    );
+    
+    //si utilizo subscribe tengo que luego anular la subscripcion
+    /*.subscribe(posts => {
+
+      console.log(posts);
+    })*/
+  }
+
+  ngOnDestroy(): void {
+
+    this.subscribe.unsubscribe();
+  }
+
+}
